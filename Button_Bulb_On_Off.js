774f568ef24e4870108dc167bcc285ad78c1a551
Prabhat Jadhav <96972995+PrabhatJadhav@users.noreply.github.com>
2022-01-02 17:27:00 +0530

    Add files via upload

---
+++ Button_Bulb_On_Off.js
@@ -0,0 +1,8 @@
+function off()
+{
+    document.getElementById("img").src="../CSS_Programs/Images/off-1426739_960_720.webp"
+}
+function on()
+{
+    document.getElementById("img").src="../CSS_Programs/Images/light-bulb.jpg"
+}
